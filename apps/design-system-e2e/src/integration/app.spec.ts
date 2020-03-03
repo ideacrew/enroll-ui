ad989d9a2e02e4ce42397d30434c70ac64b07b54
Mark Goho <markgoho@gmail.com>
2020-03-03 12:15:31 -0500

    add simple check for design-system e2e

--- apps/design-system-e2e/src/integration/app.spec.ts
+++ apps/design-system-e2e/src/integration/app.spec.ts
@@ -5,9 +5,6 @@ describe('design-system', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to design-system!');
+    cy.contains('hbx-heading-xl');
   });
 });
