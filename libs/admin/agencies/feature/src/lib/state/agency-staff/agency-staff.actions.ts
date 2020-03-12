28d02403e5335282eef34188ec761757f1f1d60a
Mark Goho <markgoho@gmail.com>
2020-03-12 13:54:07 -0400

    update action names

--- libs/admin/agencies/feature/src/lib/state/agency-staff/agency-staff.actions.ts
+++ libs/admin/agencies/feature/src/lib/state/agency-staff/agency-staff.actions.ts
@@ -6,10 +6,10 @@ import {
   AgencyStaffWithDetail,
 } from '@hbx/api-interfaces';
 
-export const loadAgencyStaff = createAction('[AgencyStaff] Load AgencyStaff');
+export const loadAgencyStaff = createAction('[Agency Staff] Load AgencyStaff');
 
 export const loadAgencyStaffSuccess = createAction(
-  '[AgencyStaff] Load Agency Staff Success',
+  '[Agency Staff] Load Agency Staff Success',
   props<{
     agencyStaff: AgencyStaff[];
   }>()
@@ -21,35 +21,35 @@ export const loadAgencyStaffFailure = createAction(
 );
 
 export const terminateAgencyRole = createAction(
-  '[Agency Staff] Terminate Agency Role',
+  '[Agency Staff List] Terminate Agency Role',
   props<{ request: RoleChangeRequest }>()
 );
 
 export const terminateAgencyRoleFailure = createAction(
-  '[Agency Staff] Terminate Agency Role Failure',
+  '[Agency Staff List] Terminate Agency Role Failure',
   props<{ request: RoleChangeRequest }>()
 );
 
 export const terminateAgencyRoleDetailPage = createAction(
-  '[Agency Detail] Terminate Agency Role',
+  '[Agency Staff Detail] Terminate Agency Role',
   props<{ request: RoleChangeRequest }>()
 );
 
 export const terminateAgencyRoleDetailPageFailure = createAction(
-  '[Agency Detail] Terminate Agency Role Failure',
+  '[Agency Staff Detail] Terminate Agency Role Failure',
   props<{ request: RoleChangeRequest }>()
 );
 
 export const clearCurrentlySelectedAgent = createAction(
-  '[Agency Detail] Clear Selected Agent'
+  '[Agency Detail List] Clear Selected Agent'
 );
 
 export const loadAgencyStaffDetailSuccess = createAction(
-  '[Agency Staff] Load Agency Staff Detail Success',
+  '[Agency Staff Detail] Load Agency Staff Detail Success',
   props<{ agencyStaff: AgencyStaffWithDetail }>()
 );
 
 export const loadAgencyStaffDetailFailure = createAction(
-  '[Agency Staff] Load Agency Staff Detail Failure',
+  '[Agency Staff Detail] Load Agency Staff Detail Failure',
   props<{ error: any }>()
 );
