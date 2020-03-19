7add12b05a563314caa7777451da65a407d51da0
Mark Goho <markgoho@gmail.com>
2020-03-19 11:28:54 -0400

    use regex to get branch name

--- post_build_results_ga.sh
+++ post_build_results_ga.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "Will post result: {\"project\":\"enroll-ui_dc\",\"branch\":\"$BRANCH\",\"sha\":\"$SHA\",\"status\":\"success\"}"
+# https://riptutorial.com/bash/example/19471/get-captured-groups-from-a-regex-match-against-a-string
+
+regex='refs/heads/(.*)'
+
+[[ "$BRANCH" =~ $regex ]]
+
+SIMPLE_BRANCH=${BASH_REMATCH[1]}
+
+echo "Will post result: {\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"success\"}"
 
 # curl -H "Content-Type: application/json" -H "X-API-Key: ${BUILD_REPORT_API_KEY}" -X POST ${BUILD_REPORT_URL} -d "{\"project\":\"enroll-ui_dc\",\"branch\":\"${TRAVIS_BRANCH}\",\"sha\":\"${TRAVIS_COMMIT}\",\"status\":\"success\"}"
 
