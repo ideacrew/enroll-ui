1f05f27f18c03158687095bfd34651d8819a140e
Mark Goho <markgoho@gmail.com>
2020-03-20 11:24:50 -0400

    try to alert yellr on failed jobs

--- post_build_results_ga.sh
+++ post_build_results_ga.sh
@@ -8,7 +8,7 @@ regex='refs/heads/(.*)'
 
 SIMPLE_BRANCH=${BASH_REMATCH[1]}
 
-echo "Will post result: {\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"passing\"}"
+echo "Will post result: {\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"$STATUS\"}"
 
 curl -H "Content-Type: application/json" -H "X-API-Key: $YELLR_KEY" -X POST $YELLR_URL -d "{\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"passing\"}"
 
