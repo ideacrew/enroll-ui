7ab673436bdd241867ca2f2508a1ec797342ab62
Mark Goho <markgoho@gmail.com>
2020-03-20 11:37:26 -0400

    use env variable for status

--- post_build_results_ga.sh
+++ post_build_results_ga.sh
@@ -10,6 +10,6 @@ SIMPLE_BRANCH=${BASH_REMATCH[1]}
 
 echo "Will post result: {\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"$STATUS\"}"
 
-curl -H "Content-Type: application/json" -H "X-API-Key: $YELLR_KEY" -X POST $YELLR_URL -d "{\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"passing\"}"
+curl -H "Content-Type: application/json" -H "X-API-Key: $YELLR_KEY" -X POST $YELLR_URL -d "{\"project\":\"enroll-ui_dc\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"$STATUS\"}"
 
 
